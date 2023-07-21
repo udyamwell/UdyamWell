febcc6819d3273f5633959a3f9b17c59d6515a6a
Kishandixit <kishandixit2000@gmail.com>
2023-07-21 08:00:38 +0530

    adding admin pannel data to db

--- udyamwell/src/components/CourseCard.jsx
+++ udyamwell/src/components/CourseCard.jsx
@@ -6,49 +6,134 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { breadcrumb } from '../assets';
 import './styles/courseCard.css';
 import PlayCircleFilledTwoToneIcon from '@mui/icons-material/PlayCircleFilledTwoTone';
-const CourseCard = () => {
-    return (
-        <>
-         <Card sx={{ maxWidth: 275 ,borderRadius:"10px"}}>
-            <Box className='cardImageBox'>
-            <CardMedia
-        component="img"
-        height="194"
-        image={breadcrumb}
-        sx={{opacity:0.8}}
-        alt="Paella dish"
-      />
-      <Box className='playIcon'>
-        <PlayCircleFilledTwoToneIcon sx={{height:"100%",width:"40%"}}/>
-      </Box>
-      <Box className='timing'>
-       <p>0.23</p>
-      </Box>
-            </Box>
-       <CardHeader
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        subheader="September 14, 2016"
-      />
-       <CardContent sx={{padding:"0 16px"}}>
-        <Typography variant="h6">
-          This impressive paeussels,
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-        </>
-    )
+// import imageType from 'image-type';
+
+// const CourseCard = ({courses}) => {
+//     return (
+//       courses.map((course)=>{
+//         <>
+//          <Card sx={{ maxWidth: 275 ,borderRadius:"10px"}}>
+//             <Box className='cardImageBox'>
+//             <CardMedia
+//         component="img"
+//         height="194"
+//         image={breadcrumb}
+//         sx={{opacity:0.8}}
+//         alt="Paella dish"
+//       />
+//       <Box className='playIcon'>
+//         <PlayCircleFilledTwoToneIcon sx={{height:"100%",width:"40%"}}/>
+//       </Box>
+//       <Box className='timing'>
+//        <p>0.23</p>
+//       </Box>
+//             </Box>
+//        <CardHeader
+//         action={
+//           <IconButton aria-label="settings">
+//             <MoreVertIcon />
+//           </IconButton>
+//         }
+//         subheader="September 14, 2016"
+//       />
+//        <CardContent sx={{padding:"0 16px"}}>
+//         <Typography variant="h6">
+//           This impressive paeussels,
+//         </Typography>
+//       </CardContent>
+//       <CardActions disableSpacing>
+//         <IconButton aria-label="add to favorites">
+//           <FavoriteIcon />
+//         </IconButton>
+//         <IconButton aria-label="share">
+//           <ShareIcon />
+//         </IconButton>
+//       </CardActions>
+//     </Card>
+//         </>
+//       })
+        
+//     )
+// }
+
+
+const CourseCard = ({ courses }) => {
+
+  const handleShare = async (title, url) => {
+    try {
+      await navigator.share({
+        title,
+        url,
+      });
+    } catch (error) {
+      console.error('Error sharing:', error);
+    }
+  };
+
+  return (
+    <>
+      {courses.map((course) => (
+        <Card key={course._id} sx={{ maxWidth: 275, borderRadius: "10px" }}>
+
+          <Box className='cardImageBox'>
+            <a href={course.link} target="_blank" rel="noopener noreferrer">
+              <CardMedia
+                component="img"
+                height="194"
+                image={breadcrumb}
+                // src={`data:image/jpeg;base64,${course.image.toString('base64')}`}
+                // src={getBlobUrl(course.image)}
+                sx={{ opacity: 0.8 }}
+                alt="Paella dish"
+              />
+              <Box className='playIcon'>
+                <PlayCircleFilledTwoToneIcon sx={{ height: "100%", width: "40%" }} />
+              </Box>
+
+              <Box className='timing'>
+                <p>0.23</p>
+              </Box>
+
+            </a>
+          </Box>
+
+          <CardHeader
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            // subheader="September 14, 2016"
+            subheader= {course.createdAt}
+          />
+
+          <CardContent sx={{ padding: "0 16px" }}>
+            <Typography variant="h6">
+              {course.name} {/* Use course.name to display the name */}
+            </Typography>
+            <Typography variant="body2">
+              {course.description} {/* Use course.description to display the description */}
+            </Typography>
+          </CardContent>
+
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share" onClick={() => handleShare(course.name, course.link)}>
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+
+        </Card>
+      ))}
+    </>
+  );
 }
+
+// const getBlobUrl = (buffer) => {
+//   const blob = new Blob([buffer], { type: imageType(buffer) });
+//   return URL.createObjectURL(blob);
+// }
 
 export default CourseCard;
--- udyamwell/src/pages/Course.jsx
+++ udyamwell/src/pages/Course.jsx
@@ -1,26 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TopSection from "../components/TopSection";
 import CourseCard from "../components/CourseCard";
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const Course = () => {
-    const navigate = useNavigate();
+  const [courses, setCourses] = useState([]);
+
+    // const navigate = useNavigate();
+
+    // const fetchData = async () => {
+    //     try {
+    //       const res = await axios.get('http://localhost:9000/users/courses');
+    //       // Handle the response data
+    //       console.log(res.data);
+    //       if(!res.status === 200){
+    //         console.log("Something Went Wrong ");
+    //       }
+    //     } catch (error) {
+    //       // Handle the error
+    //       console.error('Error fetching data:', error);
+    //       navigate('/sign-in');
+    //     }
+    //   };
 
     const fetchData = async () => {
-        try {
-          const res = await axios.get('http://localhost:9000/users/courses');
-          // Handle the response data
-          console.log(res.data);
-          if(!res.status === 200){
-            console.log("Something Went Wrong ");
-          }
-        } catch (error) {
-          // Handle the error
-          console.error('Error fetching data:', error);
-          navigate('/sign-in');
-        }
-      };
+      try {
+        const res = await axios.get(`http://localhost:9000/courses/fetch-lecture-data`);
+        // Handle the response data
+        console.log("response value =>",res.data);
+
+        setCourses(res.data);
+
+        // if(!res.status === 200){
+        //   console.log("Something Went Wrong ");
+        // }
+      } catch (error) {
+        // Handle the error
+        console.error('Error fetching data:', error);
+        // navigate('/sign-in');
+      }
+    };
     
     useEffect(()=>{
         fetchData();
@@ -29,7 +49,7 @@ const Course = () => {
         <>
          <TopSection MainHeadinig="Our Courses" subText="" text="Our Various Courses" />
          <div className="CourseContainer" style={{width:"85%",margin:"7rem auto"}}>
-         <CourseCard/>
+         <CourseCard courses={courses} />
          </div>
         </>
     )
