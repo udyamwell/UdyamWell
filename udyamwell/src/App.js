52d1c5ccf61a7c8cf79c5dab39939720f9d142bc
Dibyadyuti Kar <dibyadyutikar08@gmail.com>
2023-08-06 18:18:23 +0530

    User Email verification completed

--- udyamwell/src/App.js
+++ udyamwell/src/App.js
@@ -1,59 +1,60 @@
-import { Route, Routes, useLocation } from 'react-router';
-import './App.css';
-import Footer from './components/Footer';
+import { Route, Routes, useLocation } from "react-router";
+import "./App.css";
+import Footer from "./components/Footer";
 // import Navabar from './components/Navbar';
-import Service from './pages/Service';
-import Contact from './pages/Contact';
-import Course from './pages/Course';
-import About from './pages/About';
-import HomePage from './pages/HomePage';
-import Blog from './pages/Blog';
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
+import Course from "./pages/Course";
+import About from "./pages/About";
+import HomePage from "./pages/HomePage";
+import Blog from "./pages/Blog";
 // import HomeNavabar from './components/HomeNavbar';
-import Login from './components/Login';
-import Register from './components/Register';
-import ColorMode from './utils/ColorMode';
-import SingleCoursePage from './components/SingleCoursePage';
-import Video from './pages/admin panel/Video';
-import Profile from './pages/Profile';
-import ForgotPassword from './components/ForgotPassword';
-import ChangePassword from './components/ChangePassword';
-import NavabarMain from './components/NavbarMain';
-import VerifySucces from './components/VerifySuccess';
-import Lectures from './pages/admin panel/Lectures';
-import Users from './pages/admin panel/Users';
-import Email from './components/Email';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ColorMode from "./utils/ColorMode";
+import SingleCoursePage from "./components/SingleCoursePage";
+import Video from "./pages/admin panel/Video";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./components/ForgotPassword";
+import ChangePassword from "./components/ChangePassword";
+import NavabarMain from "./components/NavbarMain";
+import VerifySucces from "./components/VerifySuccess";
+import Lectures from "./pages/admin panel/Lectures";
+import Users from "./pages/admin panel/Users";
+import Email from "./components/Email";
 
 function App() {
   const location = useLocation();
   return (
     <>
-    <div>
-      <ColorMode>
-      {/* {(location.pathname==='/' || location.pathname==='/register' || location.pathname==='/login' || location.pathname==='/course' || location.pathname==='/profile' ) ? (<HomeNavabar/>) : (<Navabar/>)} */}
-      <NavabarMain/>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/blogs' element={<Blog/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/courses' element={<Course/>}/>
-         <Route path='/login' element={<Login/>}/>
-         <Route path='/register' element={<Email/>}/>
-        <Route path='/services' element={<Service/>}/>
-        <Route path='/course/:id' element={<SingleCoursePage/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/forgot-password' element={<ForgotPassword/>}/>
-        <Route path='/change-password/:id' element={<ChangePassword/>}/>
-        <Route path='/verify-success/:id' element={<VerifySucces/>}/>
-        <Route path='/admin'>
-            <Route path='courses' element={<Video/>}/>
-            <Route path='course/:id' element={<Lectures/>}/>
-            <Route path='users' element={<Users/>}/>
-          </Route>
-      </Routes>
-      <Footer/> 
-      </ColorMode>
-    </div>
+      <div>
+        <ColorMode>
+          {/* {(location.pathname==='/' || location.pathname==='/register' || location.pathname==='/login' || location.pathname==='/course' || location.pathname==='/profile' ) ? (<HomeNavabar/>) : (<Navabar/>)} */}
+          <NavabarMain />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blogs" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/courses" element={<Course />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Email />} />
+            <Route path="/register/:otp" element={<Register />} />
+            <Route path="/services" element={<Service />} />
+            <Route path="/course/:id" element={<SingleCoursePage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/change-password/:id" element={<ChangePassword />} />
+            <Route path="/verify-success/:id" element={<VerifySucces />} />
+            <Route path="/admin">
+              <Route path="courses" element={<Video />} />
+              <Route path="course/:id" element={<Lectures />} />
+              <Route path="users" element={<Users />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </ColorMode>
+      </div>
     </>
   );
 }
