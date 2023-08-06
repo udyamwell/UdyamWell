6ce46af379439a4acea2aaf182c978ce10216433
Dibyadyuti Kar <dibyadyutikar08@gmail.com>
2023-08-06 12:08:19 +0530

    Email verification for signup corrected

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Authenticate = require('../config/jwt_auth.js');
+const Authenticate = require("../config/jwt_auth.js");
 // const authenticate = require('../config/jwt_auth');
 
-const usersController = require('../controllers/users_controlles.js');
+const usersController = require("../controllers/users_controlles.js");
 // USER SIGN UP
-router.post('/sign-up',usersController.signUp);
+// for sending otp
+router.post("/sendOtp", usersController.sendOtp);
+router.post("/sign-up", usersController.signUp);
 
-// USER SIGN IN 
-router.post('/sign-in',usersController.signIn);
+// USER SIGN IN
+router.post("/sign-in", usersController.signIn);
 //user route for profile update
-router.put('/update',Authenticate.userAuth,usersController.updateUser);
-router.put('/forgot-password',usersController.forgotPassword);
-router.put('/change-password',usersController.changePassword);
-router.get('/verify',usersController.verifyMail);
-router.get('/',Authenticate.superAdminAuth,usersController.getAllUsers);
-router.put('/update-admin/:_id',Authenticate.superAdminAuth,usersController.updateAdmin);
-router.delete('/:_id',Authenticate.superAdminAuth,usersController.deleteUser);
+router.put("/update", Authenticate.userAuth, usersController.updateUser);
+router.put("/forgot-password", usersController.forgotPassword);
+router.put("/change-password", usersController.changePassword);
+// router.get("/verify", usersController.verifyMail);
+router.get("/", Authenticate.superAdminAuth, usersController.getAllUsers);
+router.put(
+  "/update-admin/:_id",
+  Authenticate.superAdminAuth,
+  usersController.updateAdmin
+);
+router.delete("/:_id", Authenticate.superAdminAuth, usersController.deleteUser);
 
-// ROUTER FOR COURSES PAGE 
-router.get('/courses',usersController.courses);  // ,authenticate
+// ROUTER FOR COURSES PAGE
+router.get("/courses", usersController.courses); // ,authenticate
 
 module.exports = router;
