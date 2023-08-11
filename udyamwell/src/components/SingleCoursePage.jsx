6372c24998c56d887bd6ce8ee63b4b99677768b5
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2023-08-11 19:51:44 +0530

    Update SingleCoursePage.jsx

--- udyamwell/src/components/SingleCoursePage.jsx
+++ udyamwell/src/components/SingleCoursePage.jsx
@@ -1,47 +1,65 @@
-import React, { useEffect } from "react";
-import CourseCard from './CourseCard'; 
+import React, { useEffect, useState } from "react";
+import VideoLectureCard from './VideoLectureCard';
 import './styles/courseCard.css';
 import { Box } from "@mui/material";
 import { useParams } from "react-router";
 import { fetchSingleCource } from "../slices/videos";
 import { useDispatch, useSelector } from "react-redux";
+
 const SingleCoursePage = () => {
     const {id} = useParams();
     const {courseName,lectures,error,image} = useSelector(state=>state.lectures);
     const dispatch = useDispatch();
+
     console.log("id",id)
+
+    const [selectedVideoUrl, setSelectedVideoUrl] = useState('');
+
     useEffect(()=>{
         dispatch(fetchSingleCource(id));
     },[])
-    let urlImage = `http://localhost:9000/uploads/course/thumbnails/${image}`;
+    // let urlImage = `http://localhost:9000/uploads/course/videoThumbnails/${image}`;
+    // let urlVideo = `http://localhost:9000/uploads/course/videos/`;
+
+    const handleCardClick = (videoUrl) => {
+        setSelectedVideoUrl(videoUrl);
+    };
+
+    console.log("selectdURL =====> ",selectedVideoUrl);
+
+    console.log("lectures ==>",lectures);
+
     return (
         <>
-        <div style={{marginTop:"10rem",width:"100%"}}>
-            <div >
-                <h1 style={{marginBottom:"50px",textAlign:"center"}}>{courseName}</h1>
-                <img src={urlImage} alt="" height={"100vh"} width={'80%'} style={{margin:"0 auto"}}/>
-            </div>
-            </div>
-            {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/7g4KIRP1wyY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <div className="content">
-                <h3 style={{color:"grey"}}>rgstrfhgbtd</h3>
-            </div>
-            </div>
-            {/*  */}
-            {/* <div className="recommendations">
-                <h1>Recommendations</h1>
-              <Box display={'flex'} flexWrap={'wrap'}>
-              <div className="recomVid">
-                <CourseCard/>
+        <div className="mainVidContainer">
+            <div className="mainVid" style={{marginBottom:"50px"}}>
+                <h1>{courseName}</h1>
+                {/* <video src={lectures?.[0]?.urlVideo} controls /> */}
+              <iframe
+              style={{marginTop:"26px"}}
+              width="100%"
+              height="194"
+            //   src={urlVideo+ lectures[0].video}
+              src={selectedVideoUrl}
+              title="Embedded Video Player"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+                <div className="content">
+                    <h3 style={{color:"grey"}}>rgstrfhgbtd</h3>
                 </div>
-                <div className="recomVid">
-                <CourseCard/>
-                </div>
-              </Box>
             </div>
-        </div> */}
+
+            <div className="recommendations">
+                <h1>Playlist</h1>
+                <Box display={'flex'} flexWrap={'wrap'}>
+                    {/* Render the CourseCard component passing the lectures data */}
+                    <VideoLectureCard lectures={lectures} type={'video'} onCardClick={handleCardClick}/>
+                </Box>
+            </div>
+        </div>
         </>
-    )
+    );
 }
 
 export default SingleCoursePage;
