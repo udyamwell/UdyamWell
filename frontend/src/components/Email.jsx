fd1bc6f7ea1e7c784d2b782e50e3ab01d9f4a7d4
Aman <amanchopra.atg@gmail.com>
2023-08-27 01:47:36 +0530

    experiment

--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -12,7 +12,7 @@ const Email = () => {
       alert("Please enter your registered mail");
     }
     axios
-      .post("https://udyamwell-testing.azurewebsites.net/users/sendOtp", { email: email })
+      .post("/users/sendOtp", { email: email })
       .then((res) => {
         Swal.fire(
           `${res.data.message}`,
