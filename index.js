95c958339e4a6638a7e4f7cb603e8e588b1fcaaf
Ravi <2140131@sliet.ac.in>
2023-09-22 18:42:41 +0530

    video uploading working

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const express = require('express');
 const port = 8080;
 const cors = require('cors');
 const path = require('path');
-const multer = require('multer');
-const File = require('./models/file');
 // adding monggose 
 const errorHandler = require('./config/errorHandler')
 const db = require('./config/mongoose').connect();
@@ -28,82 +26,11 @@ app.use(express.static('./frontend/build'));
 //     res.send("I am live");
 // });
 
-app.get('/lecture/:id', async(req,res)=> {
-    const id = req.params.id;
-    try {
-      const lecture = await File.findById(id);
-      return res.json({
-        lecture
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  })
-  app.get('/lecture', async (req, res) => {
-    try {
-      const lectures = await File.find();
-      return res.json({
-        lectures,
-        isError: false
-      })
-    }
-    catch (err) {
-      console.log(err);
-    }
-  })
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 })
 
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-    destination: './uploads/course/videos/',
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
-  });
-  
-  
-  const upload = multer({ storage });
-  
-  
-  
-  // Define a route for video uploads
-  const cpUpload = upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'video', maxCount: 1 }])
-  app.post('/upload', cpUpload, async (req, res) => {
-    try {
-      // const { files } = req;
-      const thumbnail = req.files.thumbnail[0];
-      const video = req.files.video[0];
-  
-      if (!thumbnail || !video) {
-        return res.status(400).json({ message: 'Please select a video file' });
-      }
-      
-      //create new File object
-      const newFile = new File({
-        title: req.body.title,
-        description: req.body.description,
-        filename: video.filename,
-        originalname: video.originalname,
-        thumbnail: thumbnail.originalname,
-        thumbnailPath: thumbnail.filename,
-        mimetype: video.mimetype,
-        size: video.size,
-        path: video.path,
-      });
-  
-      // Save the file data to MongoDB
-      await newFile.save();
-  
-      // In a real application, you would save the file information to a database
-      // For simplicity, we're just sending a success message
-      res.status(200).json({ message: 'Video uploaded successfully' });
-    } catch (error) {
-      console.error('Error uploading video:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+
 
 app.listen(port,(err)=>{
     if(err){
