ffa4af383b277d42ef1427e8d425bc7d86ad6e74
Ravi <2140131@sliet.ac.in>
2023-10-12 15:18:34 +0530

    news letter functionalities added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const newsLetter = require('../../controllers/newsLetter_email');
 
 
 router.use('/v1',require('./v1'));
+
+router.post('/newsletter',newsLetter);
 
 
 module.exports = router;
