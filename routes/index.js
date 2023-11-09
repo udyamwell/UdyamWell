19e8fc0dd39a3ac2480a89e31eb39551b57bb42b
udyamwell1 <136854759+udyamwell1@users.noreply.github.com>
2023-11-09 23:28:28 +0530

    Revert "ravi updates and fixes"

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const { createNewBlog } = require("../controllers/blogs")
-
 // router.get('/',(req,res)=>{
 //     res.send("I am Home");
 // });
@@ -17,7 +15,5 @@ router.use('/users',require('./users'));
 router.use('/courses',require('./courses'));
 
 router.use('/api',require('./api'));
-
-router.use('/blogs',require("./blogs"));
 
 module.exports = router;
